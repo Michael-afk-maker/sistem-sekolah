5de86569960ac333a567419e99e35f1f2147c9e7
Michael-afk-maker <nleo5003@gmail.com>
2026-01-27 13:14:26 +0700

    wip: implement student controller

---
+++ app/controllers/StudentsContollers.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Controllers;
+
+class StudentController
+{
+
+public function index()
+{
+   
+
+}
+
+
+
+
+public function create()
+{
+    
+
+}
+
+}
+
+
--- app/core/Router.php
+++ app/core/Router.php
@@ -7,6 +7,23 @@ class Router
 
     public function run()
     {
+        $method = $_SERVER['REQUEST_METHOD'];
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+        //https://google.com/search
+
+        if($method == 'GET' && $uri == '/students'){
+            
+            return;
+        }
+
+        if($method == 'GET' && $uri == '/students/create'){
+            
+            return;
+        }
+
+        http_response_code(404);
+        echo '<h1>404 - Page Not Found</h1>';
         
     }
     
